f74e8d59ef6d438e6687b8a683458e1e3f696a70
Developer-joaoVictor <joaoliveira.contato@hotmail.com>
2026-08-03 19:36:55 -0300

    feat(refresh token): implementacao do refresh token

--- src/http/controller/users/routes.ts
+++ src/http/controller/users/routes.ts
@@ -3,9 +3,11 @@ import { register } from './register-controller'
 import { autheticate } from './authenticate-controller'
 import { profile } from './profile-controller'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', autheticate)
+  app.patch('/token/refresh', refresh)
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
