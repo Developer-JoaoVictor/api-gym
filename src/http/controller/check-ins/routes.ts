eef491f304064bc9a7b7aa6e58dfda5db9c87287
Developer-joaoVictor <joaoliveira.contato@hotmail.com>
2026-08-03 21:38:04 -0300

    feat(autorizacao por cargo): adiciona ao projeto RBAC

--- src/http/controller/check-ins/routes.ts
+++ src/http/controller/check-ins/routes.ts
@@ -4,11 +4,16 @@ import { metrics } from './metrics'
 import { validate } from './validate'
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: verifyUserRole('ADMIN') },
+    validate,
+  )
 }
--- src/http/controller/users/authenticate-controller.ts
+++ src/http/controller/users/authenticate-controller.ts
@@ -22,7 +22,9 @@ export async function autheticate(
     })
 
     const token = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
@@ -31,7 +33,9 @@ export async function autheticate(
     )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
